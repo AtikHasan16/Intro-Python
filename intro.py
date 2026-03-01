d40e4fe043e691933dbbecfcd9b104710912d457
Hasan MD Atik <akhasannaeem@gmail.com>
2026-03-01 06:46:53 +0600

    comment out greeting and name variables in intro.py

--- intro.py
+++ intro.py
@@ -1,5 +1,9 @@
-greeting = 'hello world'
-print(greeting)
+# greeting = 'hello world'
+# print(greeting)
 
-name000 = 'hello programmer'
-print(name000)
+# name000 = 'hello programmer'
+# print(name000)
+
+sum = 323 + 23
+num = sum - 33
+print(num)
